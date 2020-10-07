fa0bf1ad47677e12a27f1b72ccb7a3539decafdd
Alessandro Diego <ALESSANDRO.DIEGO@local.ittcesena.it>
2020-10-07 11:06:30 +0200

    First Step

---
+++ SortVectors/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SortVectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vettore;
+
+            vettore = new int[10] { 4,-1,100,-85,4,6,77,-35,2,0};
+
+            for(int i=0; i < vettore.Length; i++)
+            {
+                Console.Write(vettore[i] +" | ");
+              
+            }
+            Console.ReadKey();
+        }
+    }
+}
