14183f3af455297602806f1daf4e01fb1697b3a7
Alessandro Diego <ALESSANDRO.DIEGO@local.ittcesena.it>
2020-10-07 11:24:45 +0200

    Ordinato il vettore

--- SortVectors/Program.cs
+++ SortVectors/Program.cs
@@ -9,9 +9,10 @@ namespace SortVectors
             int[] vettore;
 
             vettore = new int[10] { 4,-1,100,-85,4,6,77,-35,2,0};
-
-            for(int i=0; i < vettore.Length; i++)
+            Array.Sort(vettore);
+            for (int i=0; i < vettore.Length; i++)
             {
+                
                 Console.Write(vettore[i] +" | ");
               
             }
