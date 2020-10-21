ab4d2d83be0a8eeceea7b5ac1614f83f5b0476f4
endi.dyrmyshi <endi.dyrmyshi@PC7306B>
2020-10-21 11:20:54 +0200

    Ho fatto il metodo per il calcolo del minimo

--- SortVectors/Program.cs
+++ SortVectors/Program.cs
@@ -10,13 +10,21 @@ namespace SortVectors
 
             vettore = new int[10] { 4,-1,100,-85,4,6,77,-35,2,0};
             Array.Sort(vettore);
+            int minimo = Minimo(vettore);
+            Console.WriteLine("Il valore minimo è: " + minimo);
             for (int i=0; i < vettore.Length; i++)
             {
                 
                 Console.Write(vettore[i] +" | ");
               
             }
+            
             Console.ReadKey();
         }
+        static int Minimo(int[] vettore)
+        {
+            return vettore[0];
+        }
+
     }
 }
