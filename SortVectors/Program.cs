d21eb91b128c8a4b04dbee9710f47f1ed9b8a115
alessandro.diego <alessandro.diego@PC7305>
2020-10-21 11:20:51 +0200

    Ho inserito il metodo per il calcolo del massimo

--- SortVectors/Program.cs
+++ SortVectors/Program.cs
@@ -10,13 +10,22 @@ namespace SortVectors
 
             vettore = new int[10] { 4,-1,100,-85,4,6,77,-35,2,0};
             Array.Sort(vettore);
+            int max = Massimo(vettore);
+            Console.WriteLine("il valore massimo è: " + max);
             for (int i=0; i < vettore.Length; i++)
             {
                 
                 Console.Write(vettore[i] +" | ");
               
             }
+            
+
             Console.ReadKey();
+        }
+
+        static int Massimo(int[] v)
+        {
+            return v[9];
         }
     }
 }
